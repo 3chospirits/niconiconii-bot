4a0b3fe4e8334caf5b8b3692217dcf71fb6b4b67
3chospirits <3chospirits@gmail.com>
2021-12-31 12:40:24 -0500

    initialize github repository

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello World!")
+    }
+})
+
+client.login(process.env.TOKEN)
