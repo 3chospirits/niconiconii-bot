1395b1a1a9b17ac5ba8b297880e3cc4b52d9d9dc
LeVonara <levonara.carmine@gmail.com>
2022-02-22 14:47:02 +0100

    add: stable handlers

--- handlers/commands.js
+++ handlers/commands.js
@@ -4,18 +4,13 @@ const { Collection } = require("discord.js");
 
 module.exports = (client) => {
   client.commands = new Collection();
-  console.log("Getting the command categories");
   const categories = readdirSync("commands");
 
-  console.log("Asserting commands data dynamically into collection");
   categories.forEach((category) => {
     const commands = getFiles(`commands/${category}`, ".js");
-
     commands.forEach((commandFile) => {
       const command = require(`../commands/${category}/${commandFile}`);
-
       if (command.name && typeof command.name === "string") {
-        console.log(`[COMMAND] Successfully loaded: ${commandFile}`);
         client.commands.set(command.name, command);
       } else {
         throw new TypeError(
--- handlers/events.js
+++ handlers/events.js
@@ -3,22 +3,21 @@ const { getFiles } = require("../util/functions");
 
 module.exports = (client) => {
   client.events = new Collection();
-  console.log("Getting the client's events");
   let events = getFiles("./events/", ".js");
 
   if (events.length === 0) throw "No events was provided";
 
-  console.log("Inserting events data into collection");
   events.forEach((eventFile) => {
     const event = require(`../events/${eventFile}`);
 
     if (event.name && typeof event.name === "string") {
       client.events.set(event.name, event);
-      console.log(`[EVENT] Successfully loaded: ${eventFile}`);
     } else {
       throw new TypeError(
         `The event: ${eventFile} failed to load because it doesn't have a name property`
       );
     }
+
+    client.on(event.name, (...args) => event.run(...args));
   });
 };
