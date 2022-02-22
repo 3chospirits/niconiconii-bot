27046c0df307c9fc34fcb083c518d9a8084e146f
LeVonara <levonara.carmine@gmail.com>
2022-02-22 14:43:19 +0100

    Chore: refractor of the main file

--- index.js
+++ index.js
@@ -1,58 +1,17 @@
-const Discord = require("discord.js")
-require("dotenv").config()
-
-// const generateImage = require("./generateImage")
+const Discord = require("discord.js");
+require("dotenv").config();
 
 const client = new Discord.Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MEMBERS"
-    ]
-})
+  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
+});
 
-let bot = {
-    client, 
-    prefix: "n.",
-    owners: ["315850603396071424"]
-}
+const init = async () => {
+  const handlers = Object.values(require("./handlers/index"));
+  for (const handler of handlers) {
+    handler(client);
+  }
+};
 
-client.commands = new Discord.Collection()
-client.events = new Discord.Collection()
-client.slashcommands = new Discord.Collection()
-client.buttons = new Discord.Collection()
+init();
 
-client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
-client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
-client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
-
-client.loadEvents(bot, false)
-client.loadCommands(bot, false)
-client.loadSlashCommands(bot, false)
-client.loadButtons(bot, false)
-
-
-module.exports = bot
-
-// client.on("ready", () => {
-//     console.log(`Logged in as ${client.user.tag}`)
-// })
-
-// client.on("messageCreate", (message) => {
-//     if (message.content == "hi"){
-//         message.reply("Hello World!")
-//     }
-// })
-
-// const welcomeChannelId = "926530810008453120"
-
-// client.on("guildMemberAdd", async (member) => {
-//     const img = await generateImage(member)
-//     member.guild.channels.cache.get(welcomeChannelId).send({
-//         content: `<@${member.id}> Welcome to the server!`,
-//         files: [img]
-//     })
-// })
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
